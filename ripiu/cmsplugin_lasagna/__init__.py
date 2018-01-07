2a5a08027aa8dd370776d6cc4ef22fb8740e1550
geobaldi <geobaldi@gmail.com>
2018-01-08 00:38:01 +0100

    Forgot a migration

--- ripiu/cmsplugin_lasagna/__init__.py
+++ ripiu/cmsplugin_lasagna/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'matteo vezzola <matteo@studioripiu.it>'
 
 default_app_config = 'ripiu.cmsplugin_lasagna.apps.LasagnaConfig'
