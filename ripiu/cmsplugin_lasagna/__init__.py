c51fc4c288f42bc5ecd2def5955d00943e8e5432
geobaldi <geobaldi@gmail.com>
2018-01-10 18:46:14 +0100

    Added support for <picture>

--- ripiu/cmsplugin_lasagna/__init__.py
+++ ripiu/cmsplugin_lasagna/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __author__ = 'matteo vezzola <matteo@studioripiu.it>'
 
 default_app_config = 'ripiu.cmsplugin_lasagna.apps.LasagnaConfig'
